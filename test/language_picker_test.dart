627558cff4cb985fc01cbf30ce237ac35be2cbab
Anh Tuan <atn832@gmail.com>
2021-05-17 10:03:33 +0900

    implemented tests for Languages

--- test/language_picker_test.dart
+++ test/language_picker_test.dart
@@ -1,5 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:language_picker/languages.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('Language constants', () {
+    expect(Languages.korean.name, equals('Korean'));
+    expect(Languages.french.isoCode, equals('fr'));
+    expect(Languages.defaultLanguages.length, greaterThan(180));
+  });
 }
